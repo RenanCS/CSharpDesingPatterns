de81bf75f171b4fd19365d9264831e7b25ee50e5
Renan Carvalho <renan@interprocess.com.br>
2022-06-27 14:15:22 -0300

    feat: add prototype

---
+++ Prototype/Implementation.cs
@@ -0,0 +1,56 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    public abstract class Person
+    {
+        public abstract string Name { get; set; }
+        public abstract Person Clone(bool deepCopy);
+    }
+
+    public class Manager : Person
+    {
+        public override string Name { get; set ; }
+
+        public Manager(string name)
+        {
+            Name = name;
+        }
+
+        public override Person Clone(bool deepCopy = false)
+        {
+            if (deepCopy)
+            {
+                var objectAsJson = JsonConvert.SerializeObject(this);
+                return JsonConvert.DeserializeObject<Employee>(objectAsJson);
+            }
+            return (Person)MemberwiseClone();
+        }
+    }
+
+    public class Employee : Person
+    {
+        public Manager Manager { get; set; }
+        public override string Name { get; set; }
+        public Employee(string name, Manager manager)
+        {
+            Name = name;
+            Manager = manager;
+        }
+
+        public override Person Clone(bool deepCopy)
+        {
+            if (deepCopy)
+            {
+                var objectAsJson = JsonConvert.SerializeObject(this);
+                return JsonConvert.DeserializeObject<Employee>(objectAsJson);
+            }
+            return (Person)MemberwiseClone();
+        }
+    }
+}
---
+++ Prototype/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Prototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var manager = new Manager("Cindy");
+            var managerClone = (Manager)manager.Clone();
+            Console.WriteLine($"Manager was clone {manager.Name}");
+
+            var employee = new Employee("Kevin", manager);
+            var employeeClone = (Employee)employee.Clone();
+            Console.WriteLine($"Employee was clone {employee.Name}, with manager {employee.Manager.Name}");
+
+            Console.ReadKey();
+        }
+    }
+}
