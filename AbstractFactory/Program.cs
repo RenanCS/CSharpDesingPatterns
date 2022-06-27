6ee2292b72c7c071119803325c71ec4280efda1b
Renan Carvalho <renan@interprocess.com.br>
2022-06-27 13:10:48 -0300

    feat: add abstractfactory

---
+++ AbstractFactory/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AbstractFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var belgiumShoppingCartPurchaseFactory = new BelgiumShoppingCartPurchaseFactory();
+            var shoppingCartForBelgium = new ShoppingCart(belgiumShoppingCartPurchaseFactory);
+            shoppingCartForBelgium.CalculateCosts();
+
+            var franceShoppingCartPurchaseFactory = new FranceShoppingCartPurchaseFactory();
+            var shoppingCartFroFrance = new ShoppingCart(franceShoppingCartPurchaseFactory);
+            shoppingCartFroFrance.CalculateCosts();
+
+            Console.ReadLine();
+        }
+    }
+}
