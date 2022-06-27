5d9ce849b1449cde55df44c0959c1901aee648fa
Renan Carvalho <renan@interprocess.com.br>
2022-06-27 19:07:18 -0300

    feat: add bridge

---
+++ Bridge/Implementation.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge
+{
+    public abstract class Menu
+    {
+        public readonly ICoupon _coupon = null!;
+        public abstract int CalculatePrice();
+
+        public Menu(ICoupon coupon)
+        {
+            _coupon = coupon;
+        }
+    }
+
+    public class VegetarianMenu : Menu
+    {
+        public VegetarianMenu(ICoupon coupon): base(coupon)
+        {
+
+        }
+        public override int CalculatePrice()
+        {
+            return 20 - _coupon.CouponValue;
+        }
+    }
+
+    public class MeatBaseMenu : Menu
+    {
+        public MeatBaseMenu(ICoupon coupon) : base(coupon)
+        {
+
+        }
+        public override int CalculatePrice()
+        {
+            return 30 - _coupon.CouponValue;
+        }
+    }
+
+
+    public interface ICoupon
+    {
+        public int CouponValue { get; }
+    }
+
+    public class NoCoupon: ICoupon
+    {
+        public int CouponValue { get => 0; }
+
+    }
+
+    public class OneEuroCoupon : ICoupon
+    {
+        public int CouponValue { get => 1; }
+    }
+
+    public class TwoEuroCoupon : ICoupon
+    {
+        public int CouponValue { get => 2; }
+    }
+
+
+}
---
+++ Bridge/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Bridge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var noCoupon = new NoCoupon();
+            var oneEuroCoupoun = new OneEuroCoupon();
+
+            var meatBaseMenu = new MeatBaseMenu(noCoupon);
+            Console.WriteLine($"Meat no coupon {meatBaseMenu.CalculatePrice()}");
+
+            meatBaseMenu = new MeatBaseMenu(oneEuroCoupoun);
+            Console.WriteLine($"Meat one coupon {meatBaseMenu.CalculatePrice()}");
+
+            var vegetarianMenu = new VegetarianMenu(noCoupon);
+            Console.WriteLine($"Vegetarian no coupon {vegetarianMenu.CalculatePrice()}");
+
+            vegetarianMenu = new VegetarianMenu(oneEuroCoupoun);
+            Console.WriteLine($"Vegetarian one coupon {vegetarianMenu.CalculatePrice()}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
